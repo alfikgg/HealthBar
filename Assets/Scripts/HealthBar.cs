0b7f93b881b8ac51858698631e15ae39631228a6
alfikgg <alfikgg@gmail.com>
2020-12-10 14:56:43 +0200

    CreatRepository

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    private Slider _healthbar;
+
+    private void Start()
+    {
+        var player = _player.GetComponent<Player>();
+        _healthbar.maxValue = player.MaxHealth;
+        _healthbar.value = player.GetComponent<Player>().CurrentHealth;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+
+    public int MaxHealth { get; private set; }
+    public int CurrentHealth { get; private set; }
+
+    private void Start()
+    {
+        _maxHealth = 100;
+        MaxHealth = _maxHealth;
+        CurrentHealth = MaxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        if(CurrentHealth > 0)
+        {
+            CurrentHealth -= damage;
+        }
+    }
+}
