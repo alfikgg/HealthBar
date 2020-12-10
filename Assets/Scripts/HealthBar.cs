1694d16a3865eee517a95a603337184a57e992d5
alfikgg <alfikgg@gmail.com>
2020-12-10 16:52:29 +0200

    update

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,5 +5,27 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private Slider _healthBar;
 
+    private void Start()
+    {
+        _healthBar.maxValue = 100;
+        _healthBar.value = _healthBar.maxValue -50;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (_healthBar.value > 0)
+            _healthBar.value -= damage;
+    }
+
+    public void GetHeal(int heal)
+    {
+        if(_healthBar.value < _healthBar.maxValue)
+        {
+            _healthBar.value += heal;
+            if (_healthBar.value > _healthBar.maxValue)
+                _healthBar.value = _healthBar.maxValue;
+        }
+    }
 }
