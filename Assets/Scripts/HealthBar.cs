193504dc6b03fe47272ed97c6f7c37ff6b864c08
alfikgg <alfikgg@gmail.com>
2021-03-25 16:05:58 +0200

    try use coroutine

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _healthBar;
-    [SerializeField] private int _fillingSpeed;
+    [SerializeField] private float _fillingSpeed;
 
-    private float _targetHealth;
+    private float _targetValue;
 
     private void OnEnable()
     {
@@ -19,20 +19,29 @@ public class HealthBar : MonoBehaviour
     {
         Player.OnHealthChanged -= ChangeHealth;
     }
+
     private void Start()
     {
-        _healthBar.maxValue = _targetHealth;
-    }
-
-    private void FixedUpdate()
-    {
-        _healthBar.value = Mathf.Lerp(_healthBar.value, _targetHealth , Time.deltaTime * _fillingSpeed);
+        _healthBar.maxValue = _targetValue;    
     }
 
     private void ChangeHealth(float value)
-    {
-        _healthBar.value = _targetHealth;
-        _targetHealth = value;
+    {        
+        _healthBar.value = _targetValue;
+        _targetValue = value;
+
+        StartCoroutine(ChangeHealthValue());
     }
 
+    private IEnumerator ChangeHealthValue()
+    {
+        while(_healthBar.value != _targetValue)
+        {
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _targetValue, _fillingSpeed);
+
+            yield return null;
+        }
+
+        
+    }
 }
